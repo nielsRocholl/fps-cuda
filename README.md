143c86b7405ec25bfe1dc30ba0439ece95e9d472
nielsRocholl <nielsrocholl@gmai.com>
2024-04-10 11:09:32 +0200

    add all files

---
+++ README.md
@@ -0,0 +1,16 @@
+### FPS cuda implmentation
+
+Cuda implementation of farthers point sampling, can be used on the GPU. This code is originally from [Pointnet2_PyTorch](https://github.com/erikwijmans/Pointnet2_PyTorch) written by Erik Wijmans. 
+
+For easy incorporation into other project, I changed the setup.py such that we can create a python package that can be installed through pip. 
+
+You can now install this package through:
+```
+pip install git+https://github.com/nielsRocholl/knn-cuda/.git
+```
+
+After installing, you can import the function like:
+```
+from pointnet2_ops import pointnet2_utils
+fps_idx = pointnet2_utils.furthest_point_sample(data, npoint)
+```
---
+++ setup.py
@@ -0,0 +1,41 @@
+import glob
+import os
+import os.path as osp
+from setuptools import find_packages, setup
+from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+# Current directory and extension sources
+this_dir = osp.dirname(osp.abspath(__file__))
+_ext_src_root = osp.join("pointnet2_ops", "_ext-src")
+_ext_sources = glob.glob(osp.join(_ext_src_root, "src", "*.cpp")) + glob.glob(
+    osp.join(_ext_src_root, "src", "*.cu")
+)
+
+# Reading version from _version.py
+exec(open(osp.join("pointnet2_ops", "_version.py")).read())
+
+# Setting CUDA architectures
+os.environ["TORCH_CUDA_ARCH_LIST"] = "5.0;6.0;7.0;7.5;8.0;8.6;9.0"
+
+setup(
+    name="pointnet2_ops",
+    version='2.0.0',  
+    author="Niels Rocholl",
+    author_email="nielsrocholl@gmail.com",  
+    description="Python package for Pointnet2 operations that can run on GPU, orgiginal author: Erik Wijmans",  
+    long_description=open(osp.join(this_dir, "README.md")).read(),  
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/pointnet2_ops_lib",
+    packages=find_packages(),
+    install_requires=["torch>=1.9"], 
+    ext_modules=[
+        CUDAExtension(
+            name="pointnet2_ops._ext",
+            sources=_ext_sources,
+            extra_compile_args={"cxx": ["-O3"], "nvcc": ["-O3", "-Xfatbin", "-compress-all"]},
+            include_dirs=[osp.join(this_dir, _ext_src_root, "include")],
+        )
+    ],
+    cmdclass={"build_ext": BuildExtension},
+    include_package_data=True,
+)
